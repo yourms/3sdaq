9e05c3387d57a02b8ed68867626b9b08dbd4104f
Youngjin Son <98567808+liveAnyware@users.noreply.github.com>
2022-03-08 09:12:07 +0900

    Add files via upload

---
+++ web/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models        import *
+
+# Register your models here.
+admin.site.register(WebUser)
+admin.site.register(SBS)
+admin.site.register(Comp)
+admin.site.register(Order)
+admin.site.register(Ballance)
