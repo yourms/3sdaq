9f5d37e670f902277d86d32c7b88d06f5d8ea0b4
Min Su Jung <34851766+yourms@users.noreply.github.com>
2022-03-08 16:35:35 +0900

    Delete web/userApp directory

--- web/userApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models        import *
-
-# Register your models here.
-admin.site.register(WebUser)
-admin.site.register(SBS)
-admin.site.register(Comp)
-admin.site.register(Order)
-admin.site.register(Ballance)
