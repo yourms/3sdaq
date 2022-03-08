81644595b06f432b088401c4534a4463168fce38
Youngjin Son <98567808+liveAnyware@users.noreply.github.com>
2022-03-08 09:16:21 +0900

    Add files via upload

---
+++ web/userApp/urls.py
@@ -0,0 +1,18 @@
+from django.urls    import path , include
+from userApp        import views
+
+urlpatterns = [
+    # http://127.0.0.1:8000/user/login
+    path('index/',          views.index , name='index'),
+    path('login/',          views.login),
+    path('list/' ,          views.list),
+    path('detail/' ,        views.detail),
+    path('registerForm/' ,  views.registerForm),
+    path('join/' ,          views.join),
+    path('logout/' ,          views.logout),
+    path('my_order/' ,          views.my_order),
+
+]
+
+
+
