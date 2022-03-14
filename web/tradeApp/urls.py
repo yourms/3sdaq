245a475217d4777253040c30fdd03bfcc8b3aae8
Min Su Jung <34851766+yourms@users.noreply.github.com>
2022-03-14 18:01:43 +0900

    프린트문 수정
    프린트문 수정하였습니다.

--- web/tradeApp/urls.py
+++ web/tradeApp/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from tradeApp import views
-print("tra"
-      ""
-      "deApp urls")
+print("tradeApp urls")
 urlpatterns = [
     # http://127.0.0.1:8000/trade/index
     path('index/', views.index, name='index'),
