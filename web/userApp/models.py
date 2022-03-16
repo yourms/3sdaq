c3fc276c9d81a84a2e288c1a318e8a0a86cc5155
park <parkhyun0924@naver.com>
2022-03-16 13:13:37 +0900

    userApp models.py --> 주석 달았습니당! # 보현 test event log 뜨나용? 주석

--- web/userApp/models.py
+++ web/userApp/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 # class - table
 # create table webuser ( user_id varchar2(100) )
+
+
+# 보현 test event log 뜨나용? 주석
+
 class WebUser(models.Model) :
     user_id      = models.TextField(max_length=100)
     user_pwd     = models.TextField(max_length=100)
